62c5d6510a5567e7ce836a79c8c4e0ce5f9cd8e7
pouroaf-cpu <pouroaf@gmail.com>
2025-08-25 14:03:22 +1200

    letter.js

---
+++ gp-Letter/letter.js
@@ -0,0 +1,4 @@
+export function generateLetter({ bmi, waistRatio, sleep, energy, libido, stress, activity, email }) {
+  let letter = `Dear Dr.,
+
+I am submitting my health information for review. Please consider evaluating the
