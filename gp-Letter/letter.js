cabd8c66fb98579aad29cab3b972a0bee253175d
pouroaf-cpu <pouroaf@gmail.com>
2025-11-22 20:08:23 +1300

    Delete gp-Letter directory

--- gp-Letter/letter.js
+++
@@ -1,4 +0,0 @@
-export function generateLetter({ bmi, waistRatio, sleep, energy, libido, stress, activity, email }) {
-  let letter = `Dear Dr.,
-
-I am submitting my health information for review. Please consider evaluating the
